367989ffb643c79b77d2ef5eefe827894925e11c
LIAO-wanting <71537228+LIAO-wanting@users.noreply.github.com>
2021-08-12 17:09:14 +0800

    Update main5.js

--- src/pgzhelper/main5.js
+++ src/pgzhelper/main5.js
@@ -7,21 +7,21 @@ var $builtinmodule = function (name) {
 	var pgzero = Sk.importModule("pgzrun", false, true);
 
 	mod.Actor = Sk.misceval.buildClass(mod, function($gbl, $loc) {
-	//构造器函数
-	$loc.__init__ = new Sk.builtin.func(function(self, image, pos, anchor, kwargs) {
-	    self._flip_x = false
-	    self._flip_y = false
-	    self._scale = 1
-	    self._mask =null
-	    self._animate_counter = 0
-	    self.fps = 5
-	    self.direction = 0
-	});
-	$loc.direction_to=new Sk.builtin.func(function(self,) {
+		//构造器函数
+		$loc.__init__ = new Sk.builtin.func(function(self, image, pos, anchor, kwargs) {
+		    self._flip_x = false
+		    self._flip_y = false
+		    self._scale = 1
+		    self._mask =null
+		    self._animate_counter = 0
+		    self.fps = 5
+		    self.direction = 0
+		});
+		$loc.direction_to=new Sk.builtin.func(function(self) {
 
-	});
+		});
 	}, "Actor");
 	Sk.abstr.setUpInheritance("Actor", mod.Actor, pgzero.Actor)
 
-	return pgzhelper;
+	return mod;
 }
