364c1128c3337380720e2b1c3e3b771ccd7ecd0c
LIAO-wanting <71537228+LIAO-wanting@users.noreply.github.com>
2021-08-12 14:32:22 +0800

    Update and rename main1.js to main2.js

---
+++ src/pgzhelper/main2.js
@@ -0,0 +1,22 @@
+var $builtinmodule = function (name) {
+	var pgzhelper= {__name__: new Sk.builtin.str("pgzhelper")};
+    //导入外部引入的库
+    var math = Sk.importModule("math", false, false); 
+    // var sys = Sk.importModule("sys", false, false);
+    var time = Sk.importModule("time", false, false);
+    var pgzero = Sk.importModule("pgzrun", false, false);
+
+    // var mod=sys.modules['__main__'];
+    // var _fullscreen=false;
+    console.log("success")
+    //函数：set_fullscreen()
+    //设为全屏模式
+	// pgzhelper.set_fullscreen = new Sk.builtin.func(function() {
+    //     Sk.builtin.pyCheckArgs("set_fullscreen", arguments, 0, 0);
+    //     mod.screen.surface =Sk.misceval.callsim(pygame.$d['display.set_mode'],Sk.ffi.remapToPy((mod.WIDTH, mod.HEIGHT), pygame.FULLSCREEN));
+    //     _fullscreen =true
+    // });
+    
+
+	return pgzhelper;
+}
