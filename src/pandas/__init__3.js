c247c5e28e32440ad12b9bff76d24bcc99ee7a31
LIAO-wanting <71537228+LIAO-wanting@users.noreply.github.com>
2021-09-01 09:42:25 +0800

    Update and rename __init__2.js to __init__3.js

--- src/pandas/__init__3.js
+++ src/pandas/__init__3.js
@@ -2,7 +2,7 @@ var $builtinmodule = function (name) {
 	let mod= {__name__: new Sk.builtin.str("pandas")};
     
     // func: pandas.Series()
-    let Series_f = function (dict,index) {
+    mod.Series=new Sk.builtin.func(function(dict,index) {
         // Parse arguments
         Sk.builtin.pyCheckArgs("Series", arguments, 1, 2, true, false);
         var dict= Sk.ffi.remapToJs(dict)
@@ -20,10 +20,7 @@ var $builtinmodule = function (name) {
             s.print()
             return Sk.ffi.remapToPy(s)
         }
-
-    };
-    Series_f.co_kwargs = true;
-    mod.Series = new Sk.builtin.func(Series_f);
+    })
 
 	return mod;
 }
