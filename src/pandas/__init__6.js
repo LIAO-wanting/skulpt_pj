22b5a483ac3f0df7056faad30eca1d249918055f
LIAO-wanting <71537228+LIAO-wanting@users.noreply.github.com>
2021-09-01 10:43:10 +0800

    Update and rename __init__5.js to __init__6.js

--- src/pandas/__init__6.js
+++ src/pandas/__init__6.js
@@ -6,19 +6,14 @@ var $builtinmodule = function (name) {
         // Parse arguments
         var dict= Sk.ffi.remapToJs(dict)
         var dict_index= Sk.ffi.remapToJs(index) || [];
+        var s="error"
         if(dict_index.length==0){//没有传入index值
-            console.log("1")
-            console.log(dict_index)
-            var s = new dfd.Series(dict)
-            s.print()
-            return Sk.ffi.remapToPy(s.print())
+            s = new dfd.Series(dict)
         }else{
-            console.log("2")
-            console.log(dict_index)
-            var s = new dfd.Series(dict, {index:dict_index })
-            s.print()
-            return Sk.ffi.remapToPy(s.print())
+            s = new dfd.Series(dict, {index:dict_index })
         }
+        s.print()
+        return Sk.ffi.remapToPy(s)
     })
 
 	return mod;
