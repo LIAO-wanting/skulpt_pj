1b7be236cade96a732d2e9515b216a1a613e5db3
LIAO-wanting <71537228+LIAO-wanting@users.noreply.github.com>
2021-09-12 21:56:26 +0800

    Create numpy_new.js

---
+++ src/numpy/numpy_new.js
@@ -0,0 +1,32 @@
+var $builtinmodule = function (name) {
+	let mod= {__name__: new Sk.builtin.str("numpy")};
+    
+    // func: numpy.arange
+    var arange_f=function(start, stop, step) {
+		Sk.builtin.pyCheckArgs("arange", arguments, 1, 3);
+        Sk.builtin.pyCheckType("start", "number", Sk.builtin.checkNumber(start));
+        var start_num;
+        var stop_num;
+        var step_num;
+
+        if (stop === undefined && step === undefined) {
+            start_num = 0;
+            stop_num = Sk.ffi.remapToJs(start);
+            step_num = Sk.ffi.remapToJs(1);
+        } else if (step === undefined) {
+            start_num = Sk.ffi.remapToJs(start);
+            stop_num = Sk.ffi.remapToJs(stop);
+            step_num = 1;
+        } else {
+            start_num = Sk.ffi.remapToJs(start);
+            stop_num = Sk.ffi.remapToJs(stop);
+            step_num = Sk.ffi.remapToJs(step);
+        }
+        // 返回生成的array
+        var arange_buffer = math.range(start_num, stop_num, step_num);
+        return Sk.ffi.remapToPy(arange_buffer)
+	}
+	mod.arange = new Sk.builtin.func(arange_f);
+    
+	return mod;
+}
