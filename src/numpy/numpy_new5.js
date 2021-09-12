84e03871b96ff091dc2e9eec7093424f4cbfe76b
LIAO-wanting <71537228+LIAO-wanting@users.noreply.github.com>
2021-09-12 23:01:44 +0800

    Update and rename numpy_new4.js to numpy_new5.js

--- src/numpy/numpy_new5.js
+++ src/numpy/numpy_new5.js
@@ -24,7 +24,9 @@ var $builtinmodule = function (name) {
         }
         // 返回生成的array
         var arange_buffer = math.range(start_num, stop_num, step_num);
-        return Sk.ffi.remapToPy(arange_buffer.v);
+        console.log(arange_buffer)
+        console.log(Sk.ffi.remapToPy(arange_buffer)['_data'])
+        return Sk.ffi.remapToPy(arange_buffer);
 	}
 	mod.arange = new Sk.builtin.func(arange_f);
     
