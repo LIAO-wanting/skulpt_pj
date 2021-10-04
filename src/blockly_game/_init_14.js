4cf8e20e17ffe253d34466e26052ac766d2f8a48
TT <71537228+LIAO-wanting@users.noreply.github.com>
2021-10-04 12:10:40 +0800

    Update and rename _init_13.js to _init_14.js

--- src/blockly_game/_init_14.js
+++ src/blockly_game/_init_14.js
@@ -473,15 +473,14 @@ var $builtinmodule = function (name) {
             }));
         });
         $loc.isDone=new Sk.builtin.func(function(self,id){
-            return new Sk.misceval.promiseToSuspension(new Promise(function(resolve) {
+            var promise=new Sk.misceval.promiseToSuspension(new Promise(function(resolve) {
                 Sk.setTimeout(function() {
                     Sk.builtin.pyCheckArgs("isDone", arguments, 2, 2);
                     highlight(id)
-                    return Sk.ffi.remapToPy(checkFinish())
-                    // resolve(Sk.builtin.none.none$);
+                    resolve(Sk.builtin.none.none$);
                 }, 800);
-                
             }));
+            return Sk.ffi.remapToPy(checkFinish())
         });
         $loc.isPath=new Sk.builtin.func(function(self,direction,id){
             Sk.builtin.pyCheckArgs("isPath", arguments, 3, 3);
