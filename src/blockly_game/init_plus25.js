cadc62a0886247fa4199707a8e850ab59a9af8cf
TT <71537228+LIAO-wanting@users.noreply.github.com>
2021-10-17 09:08:22 +0800

    Update and rename init_plus24.js to init_plus25.js

--- src/blockly_game/init_plus25.js
+++ src/blockly_game/init_plus25.js
@@ -431,7 +431,7 @@ var $builtinmodule = function (name) {
 
         if((map[Pos_y-1][Pos_x-1]==2)||(map[Pos_y-1][Pos_x-1]==3)){
             throw Error("错误！不能将放置物位置设置在起点或终点坐标！")
-        }else if((Pos_x>(map[0].length-1)) || (Pos_x< 0) || (Pos_y>(map.length-1)) || (Pos_y< 0)){
+        }else if((Pos_x>(map[0].length)) || (Pos_x< 0) || (Pos_y>(map.length)) || (Pos_y< 0)){
             throw Error("错误！放置物坐标超过地图范围")
         }
         switch(type){
