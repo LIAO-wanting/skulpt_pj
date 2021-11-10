a42d1ae282340133c9f44a5530f557bacf30b509
TT <71537228+LIAO-wanting@users.noreply.github.com>
2021-11-10 09:37:22 +0800

    Update and rename bg_nonehl57.js to bg_nonehl58.js

--- src/bg_forbook/bg_nonehl58.js
+++ src/bg_forbook/bg_nonehl58.js
@@ -499,10 +499,10 @@ var $builtinmodule = function (name) {
                         svg.append('image').attr('id','trafficlight').attr('x',x * maze_SQUARE_SIZE+ (maze_SQUARE_SIZE/2 - maze_SQUARE_SIZE*0.7/2)).attr('y',y * maze_SQUARE_SIZE+ (maze_SQUARE_SIZE/2 - maze_SQUARE_SIZE*0.7/2)).attr('width',maze_SQUARE_SIZE*0.7).attr('height',maze_SQUARE_SIZE*0.7)
                         .attr('xlink:href','https://cdn.jsdelivr.net/gh/LIAO-wanting/skulpt_pj@main/pic/book/trafficlight.png')
                     }else if(map[y][x]==22){//当地图中此处标记为22——红绿灯中的绿灯时
-                        svg.append('image').attr('id','lightgreen').attr('x',x * maze_SQUARE_SIZE-10).attr('y',y * maze_SQUARE_SIZE+ 10).attr('width',maze_SQUARE_SIZE).attr('height',maze_SQUARE_SIZE)
+                        svg.append('image').attr('id','lightgreen').attr('x',x * maze_SQUARE_SIZE-5).attr('y',y * maze_SQUARE_SIZE+ 5).attr('width',maze_SQUARE_SIZE).attr('height',maze_SQUARE_SIZE)
                         .attr('xlink:href','https://cdn.jsdelivr.net/gh/LIAO-wanting/skulpt_pj@main/pic/book/greenlight.png')
                     }else if(map[y][x]==23){//当地图中此处标记为23——红绿灯中的红灯时
-                        svg.append('image').attr('id','lightred').attr('x',x * maze_SQUARE_SIZE-10).attr('y',y * maze_SQUARE_SIZE+ 10).attr('width',maze_SQUARE_SIZE).attr('height',maze_SQUARE_SIZE)
+                        svg.append('image').attr('id','lightred').attr('x',x * maze_SQUARE_SIZE-5).attr('y',y * maze_SQUARE_SIZE+ 5).attr('width',maze_SQUARE_SIZE).attr('height',maze_SQUARE_SIZE)
                         .attr('xlink:href','https://cdn.jsdelivr.net/gh/LIAO-wanting/skulpt_pj@main/pic/book/redlight.png')
                     }else if(map[y][x]==2){//当地图中此处标记为起点时，画上和“既是起点又是终点”一样的图标
                         actor.x= x;
@@ -909,7 +909,7 @@ var $builtinmodule = function (name) {
                             actor.traffic_light=map[actor.y][actor.x+1];
                             if(actor.traffic_light==maze.SquareType.LIGHT_RED){//图像变为红灯
                                 d3.select("#lightgreen").remove();
-                                svg.append('image').attr('id','lightred').attr('x',(actor.x+1) * maze_SQUARE_SIZE+10).attr('y',actor.y * maze_SQUARE_SIZE-10).attr('width',maze_SQUARE_SIZE).attr('height',maze_SQUARE_SIZE)
+                                svg.append('image').attr('id','lightred').attr('x',(actor.x+1) * maze_SQUARE_SIZE-5).attr('y',actor.y * maze_SQUARE_SIZE+5).attr('width',maze_SQUARE_SIZE).attr('height',maze_SQUARE_SIZE)
                                 .attr('xlink:href','https://cdn.jsdelivr.net/gh/LIAO-wanting/skulpt_pj@main/pic/book/redlight.png')
                             }   
                         }
