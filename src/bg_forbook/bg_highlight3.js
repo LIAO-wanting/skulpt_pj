4508dd2df27b1f8bada0214e8e9814668b77ad92
TT <71537228+LIAO-wanting@users.noreply.github.com>
2021-11-18 10:31:24 +0800

    Update and rename bg_highlight2.js to bg_highlight3.js

--- src/bg_forbook/bg_highlight3.js
+++ src/bg_forbook/bg_highlight3.js
@@ -860,6 +860,7 @@ var $builtinmodule = function (name) {
             // Do things
             setTimeout( () => {   
                 var state=false;
+                var square=0;
                 var re=/block_id=([\s\S]*)/.exec(block_id)
                 if(re!=null){
                     block_id=re[1];
@@ -868,16 +869,32 @@ var $builtinmodule = function (name) {
                 highlight(block_id);
                 switch (direction) {
                     case DirectionType.NORTH:
-                        square = map[actor.y - 1] && map[actor.y - 1][actor.x];
+                        if(map[actor.y - 1]){
+                            square = map[actor.y - 1][actor.x];
+                        }else{
+                            square = 0
+                        }
                         break;
                     case DirectionType.EAST:
-                        square = map[actor.y][actor.x + 1];
+                        if(map[actor.y][actor.x + 1]){
+                            square = map[actor.y][actor.x + 1];
+                        }else{
+                            square = 0
+                        }
                         break;
                     case DirectionType.SOUTH:
-                        square = map[actor.y + 1] && map[actor.y + 1][actor.x];
+                        if(map[actor.y + 1]){
+                            square = map[actor.y + 1][actor.x];
+                        }else{
+                            square = 0
+                        }
                         break;
                     case DirectionType.WEST:
-                        square = map[actor.y][actor.x - 1];
+                        if(map[actor.y][actor.x - 1]){
+                            square = map[actor.y][actor.x - 1];
+                        }else{
+                            square = 0
+                        }
                         break;
                 };
                 state= square == maze.SquareType.BARRIER;
