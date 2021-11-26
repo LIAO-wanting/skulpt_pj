54fb00d756e40a935326e9c19ff63f7bd5870b13
TT <71537228+LIAO-wanting@users.noreply.github.com>
2021-11-26 08:33:14 +0800

    Update and rename bg_nonehl15.js to bg_nonehl16.js

--- src/MixToy/bg_nonehl16.js
+++ src/MixToy/bg_nonehl16.js
@@ -970,7 +970,7 @@ var $builtinmodule = function (name) {
                             alert("挑战成功！");
                         },1000)
                         // resolve(Sk.builtin.none.none$);
-                        throw new Sk.builtin.Error("挑战成功！");
+                        throw Error("挑战成功！");
                     }else if(state=="error2"){
                         maze.result=ResultType.FAILURE
                         alert("挑战失败，请检查是否通过所有标记点！")
